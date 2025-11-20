1819fca6aedfb9f62c8ce90d2076d10048b914ed
JD95 <jeffreydwyer95@outlook.com>
2025-11-20 14:29:42 -0800

    Adds flake to build site

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "Giraffic Jams Site";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+  };
+
+  outputs = { self, nixpkgs }: let 
+    system = "x86_64-linux"; 
+    pkgs = nixpkgs.legacyPackages.${system};
+  in {
+    packages.${system}.default = pkgs.stdenv.mkDerivation {
+      pname = "giraffic-jams-site";
+      version = "0.0.1";
+
+      src = ./site;
+
+      installPhase = ''
+        mkdir -p $out
+        cp -r ./* $out/
+      '';
+    };
+  };
+}
