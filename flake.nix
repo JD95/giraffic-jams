a16408d20fe3d30eccf562c640988fd2da2988ca
JD95 <jeffreydwyer95@outlook.com>
2025-11-20 15:10:52 -0800

    Adds default package

--- flake.nix
+++ flake.nix
@@ -9,6 +9,7 @@
     system = "x86_64-linux"; 
     pkgs = nixpkgs.legacyPackages.${system};
   in {
+    packages.${system}.default = self.site;
     site = pkgs.stdenv.mkDerivation {
       pname = "giraffic-jams-site";
       version = "0.0.1";
